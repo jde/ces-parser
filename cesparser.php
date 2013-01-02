9b495feacf4b66f4b35290a08d58281ac903ebf9
David Erwin <davidjerwin@gmail.com>
2013-01-02 13:54:33 -0500

    dev(parse): added meta data to output

--- cesparser.php
+++ cesparser.php
@@ -1,6 +1,46 @@
 <?php
 header('Content-type: application/json');
 
+function getHeader($line) {
+
+	$ts = explode("  ", $line);
+	$x = 0;
+	while ($ts[$x] == "") {
+		$x++;
+	}
+	return $section = str_replace(":", "", str_replace(".", "", trim($ts[$x])));
+}
+
+function metafy($lines) {
+
+	$section = "";
+	$subsection = "";
+
+	foreach ($lines as $i => $line) {
+
+		if ($i < 10) continue;
+
+		if (substr($line, 0, 1) != " ") {
+			$section = getHeader($line);
+			$subsection = "";
+		}
+
+		if (substr($line, 0, 2) != "  ") {
+			$subsection = getHeader($line);
+		}
+
+		$lines[$i] = array(
+			'section' => $section,
+			'subsection' => $subsection,
+			'text' => $line
+		);
+
+	}
+
+	return $lines;
+
+}
+
 function tokenize($str, $sep = "  ", $stripEmpty = true) {
 
 	$tokens = explode($sep, $str);
@@ -46,15 +86,21 @@ function getBlsData($url) {
 			continue;
 		}
 
-		// tokenize
-		$lines[$i] = tokenize($line);
+	}
 
+	$lines = metafy($lines);
+
+	foreach ($lines as $i => $line) {
+
+		@$lines[$i]['tokens'] = tokenize($line['text']);
 	}
 
 	// Step 1 - get the titile
 	$data['title'] = '';
-	while (count($line = array_shift($lines)) === 1) {
+	$line = array_shift($lines);
+	while (count($line['tokens']) === 1) {
 		$data['title'] .= $line[0];
+		$line = array_shift($lines);
 	}
 	array_unshift($lines, $line); // put that last one back on ;)
 
@@ -64,29 +110,29 @@ function getBlsData($url) {
 	$g1 = array_shift($lines);
 	$g2 = array_shift($lines);
 	$g3 = array_shift($lines);
-	foreach ($g1 as $i => $g) {
+	foreach ($g1['tokens'] as $i => $g) {
 		$data['groups'][$i] = array(
-			'text' => @$g1[$i] . " " . @$g2[$i] . " " . @$g3[$i],
-			'low' => $g1[$i],
-			'high' => $g3[$i]
+			'text' => @$g1['tokens'][$i] . " " . @$g2['tokens'][$i] . " " . @$g3['tokens'][$i],
+			'low' => $g1['tokens'][$i],
+			'high' => $g3['tokens'][$i],
 		);
 	}
 
 	// Step 3 - charge through and get the lines of data
 	while (is_array($line = array_shift($lines))) {
 
-		if (count($line) == 1) {
+		if (count($line['tokens']) == 1) {
 //			echo $line[0];
 			continue;
 		}
 
 		$thisData = array();
-		foreach ($line as $i => $token) {
+		foreach ($line['tokens'] as $i => $token) {
 			if (! $i) continue;
 			array_push($thisData, preg_replace("/[^0-9]/", "", $token));
 		}
 
-		$data[$line[0]] = $thisData;
+		$data[$line['tokens'][0]] = array('values' => $thisData, 'meta' => $line);
 
 
 	}
